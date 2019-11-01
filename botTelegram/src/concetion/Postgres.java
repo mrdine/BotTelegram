deef305f7d0b85daf825e4814a96ec203066e979
danie <danie@DESKTOP-2QS4JAO>
2019-10-31 23:53:02 -0300

    first commit

---
+++ botTelegram/src/concetion/Postgres.java
@@ -0,0 +1,37 @@
+package concetion;
+
+import java.sql.*;
+
+public class Postgres {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (java.lang.ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        String url = "jdbc:postgresql://"+"tuffi.db.elephantsql.com"+":5432"+"/fcrrwpsq";
+        String username = "fcrrwpsq";
+        String password = "7nhXk7OI2taPsBUUd02RvPxGAJjItWjC";
+
+        try {
+            Connection db = DriverManager.getConnection(url, username, password);
+            Statement st = db.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM localizacao");
+            while (rs.next()) {
+                System.out.print("Column 1 returned ");
+                System.out.println(rs.getString(2));
+                System.out.print("Column 2 returned ");
+                System.out.println(rs.getString(3));
+            }
+            rs.close();
+            st.close();
+            }
+        catch (java.sql.SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
