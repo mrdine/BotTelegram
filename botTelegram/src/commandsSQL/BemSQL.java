f0aaa11d1d393c0957d7f884ddadd0f5cd10f4d6
Elvis Melo <elvismedeiros.mm3@gmail.com>
2019-11-01 17:24:43 -0300

    Documentação da classe 'BemSQL' para teste

--- botTelegram/src/commandsSQL/BemSQL.java
+++ botTelegram/src/commandsSQL/BemSQL.java
@@ -1,46 +1,90 @@
 package commandsSQL;
 
 import interfaces.PostgreSQLCommands;
-
+/**
+ * Classe que faz as operações dos bens de acordo com as chamadas SQL
+ * 
+ * @author Danielvis
+ * 
+ * */
 public class BemSQL implements PostgreSQLCommands {
 	
+	/**
+	 * Atributos de um bem
+	 * */
 	private String nome;
 	private String descricao;
 	private int localizacao;
 	private int categoria;
 	
+	/**
+	 * Retorna o nome do bem
+	 * @param void
+	 * @return String nome
+	 */
 	public String getNome() {
 		return nome;
 	}
 
+	/**
+	 * Adiciona o nome do bem
+	 * @param nome New nome
+	 */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
+	
+	/**
+	 * Retorna a descrição do bem
+	 * @return String descricao
+	 */
 	public String getDescricao() {
 		return descricao;
 	}
 
+	/**
+	 * Adiciona a descrição do bem
+	 * @param descricao New descrição
+	 */
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
+	/**
+	 * Retorna a localização do bem
+	 * @return int localizaçãoo
+	 */
 	public int getLocalizacao() {
 		return localizacao;
 	}
+	
+	/**
+	 * Adiciona a localização do bem
+	 * @param localizacao New localização
+	 */
 
 	public void setLocalizacao(int localizacao) {
 		this.localizacao = localizacao;
 	}
 
+	/**
+	 * Retorna a categoria do bem
+	 * @return int categoria
+	 */
 	public int getCategoria() {
 		return categoria;
 	}
 
+	/**
+	 * Adiciona a categoria do bem
+	 * @param categoria New categoria
+	 */
 	public void setCategoria(int categoria) {
 		this.categoria = categoria;
 	}
-
+	/**
+	 * Insere um novo bem
+	 */
 	@Override
 	public void inserir() {
 		
@@ -48,6 +92,9 @@ public class BemSQL implements PostgreSQLCommands {
 		
 	}
 
+	/**
+	 * Lista os bens de acordo com parâmetros passados
+	 */
 	@Override
 	public void listar() {
 		// TODO Auto-generated method stub
